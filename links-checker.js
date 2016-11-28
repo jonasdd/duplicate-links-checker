fcd8fc84452bed35414f441bd66350e75c9cfc70
jonasdd <jonas.brecht@gmail.com>
2016-11-28 13:01:10 +0100

    Create links-checker.js

---
+++ links-checker.js
@@ -0,0 +1,19 @@
+  var as = document.getElementsByTagName('a');
+    //  var truc;
+    var links = {};
+    for (var i = 0; i < as.length; i++) {
+      if (as[i].childNodes[0].nodeName === '#text'){
+        l = as[i].innerHTML;
+        if(!links[l]){
+          links[l] = [];
+        }
+        links[l].push(as[i]);
+      }
+      console.log(as[i])
+    }
+    
+    Object.keys(links).forEach(function(key){
+      if (links[key].length > 1) {
+          console.log("Intitulé de lien dupliqué : "+key, links[key]);
+      }
+    })
