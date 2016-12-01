f9a3d977888d0a2ab4d6818f0ebf7e44e9a7b9ea
jonasdd <jonas.brecht@gmail.com>
2016-12-01 12:41:53 +0100

    Update and rename links-checker-0.2.js to links-checker-0.3.js

---
+++ links-checker-0.3.js
@@ -0,0 +1,31 @@
+var as = document.getElementsByTagName('a');
+var links = {};
+for (var i = 0; i < as.length; i++) {
+  if (as[i].childNodes.length != 0){
+    console.log(as[i].childNodes[0]);
+    // on filtre par liens simple (pas de liens contenant d'autres info). Les liens vide ne sont pas remontés, ni les liens avec title identique, ni les liens avec le même href
+    if (as[i].childNodes[0].nodeName === '#text'){
+      l = as[i].innerHTML;
+      if(!links[l]){
+        links[l] = [];
+      }
+      links[l].push(as[i]);
+    }
+  }
+}
+
+var doublonCount = 0;
+Object.keys(links).forEach(function(key){
+  var autoColor = Math.floor(Math.random() *  210) + 1;
+  doublonCount++;
+  if (links[key].length > 1) {
+      console.log("Intitulé de lien dupliqué : "+key, links[key]);
+      for (var i = 0; i < links[key].length; i++) {
+        var span = document.createElement('span');
+        span.setAttribute("style","border:1px solid gray;color:black;font-size:0.875em;background:lightgray;position:absolute;left:-4px;bottom:100%;padding:2px");
+        links[key][i].setAttribute('style','border: 4px solid hsl('+autoColor+', 100%, 50%);position:relative');
+        links[key][i].appendChild(span);
+        span.innerHTML= "#"+doublonCount;
+      }
+  }
+})
